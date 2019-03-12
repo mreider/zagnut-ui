f5e3634243599824fbb061d312dca31b35b19eef
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.local>
2019-03-12 20:01:28 +0300

    developing url changed

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BACKEND_URL: '"http://localhost:3000"'
+  BACKEND_URL: '"http://6sprints.com"'
+  // BACKEND_URL: '"http://localhost:3000"'
 });
