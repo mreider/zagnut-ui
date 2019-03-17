f2b887ccaa67e7069a030d44b6e37d0670f9f6f8
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.Dlink>
2019-03-17 20:50:27 +0300

    loading  indicator added

---
+++ src/components/shared/LoadingIndication.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-xs-center preloader-holder">
+    <v-progress-circular :size="50" color="primary" indeterminate></v-progress-circular>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loading-indication",
+  props: ["loading"]
+};
+</script>
+
+<style lang="scss">
+.preloader-holder {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 100%;
+  height: 80%;
+  z-index: 10000;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+.v-progress-circular {
+  margin: 0 auto;
+  margin-top: 15%;
+}
+</style>
