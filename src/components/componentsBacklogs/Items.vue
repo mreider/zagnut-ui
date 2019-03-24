00cc96f432fb2957830ec725c014e3080c8df78b
Ильнур Ибрагимов <ilnuribragimov@Mac-mini-Ilnur.Dlink>
2019-03-24 11:33:58 +0300

    develop filter

--- src/components/componentsBacklogs/Items.vue
+++ src/components/componentsBacklogs/Items.vue
@@ -215,6 +215,199 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <b-card bg-variant="light" class="card col-lg-12">
+      <div class="Items">
+        <div class="row">
+          <label class="header col-10">
+            <h2>{{ this.title }}</h2>
+          </label>
+          <div class="col-2">
+            <b-btn class="float-right btnHeader" variant="primary" size="sm" v-b-modal.modalnew>New</b-btn>
+            <b-btn
+              class="float-right btnHeader"
+              variant="secondary"
+              size="sm"
+              @click="$router.go(-1)"
+            >close</b-btn>
+          </div>
+
+          <div class="col-6">
+            <b-form-checkbox
+              style="float-right"
+              id="checkbox0"
+              v-model="showArchived"
+              @change="reload"
+            >Show archived</b-form-checkbox>
+          </div>
+
+          <div class="col-6">
+            <b-form-group label="Show: " label-for="statuses" class="float-right">
+              <b-form-checkbox-group
+                id="statuses"
+                name="statuses"
+                v-model="selected"
+                :options="options"
+                class="float-right"
+              ></b-form-checkbox-group>
+            </b-form-group>
+          </div>
+
+          <div class="col-6"></div>
+          <b-input-group class="col-6">
+            <b-form-input size="sm" v-model="filter" style="margin-top:5px;" placeholder="Filter"/>
+            <b-input-group-append>
+              <b-btn size="sm" class="btnHeader" :disabled="!filter" @click="filter = ''">Clear</b-btn>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+        <div v-for="element in selected" v-bind:key="element.id" class="row">
+          <h6>{{ element.name }}</h6>
+          <b-table
+            bordered
+            fixed
+            responsive
+            :items="element.filteredItems"
+            :fields="itemsFields"
+            thead-class="hidden_header"
+            :filter="filter"
+          >
+            <template slot="title" slot-scope="data" class="col-8">
+              <router-link
+                :to="'item/?orgId='+$store.state.organization.id +'&itemId='+ data.item.id"
+                v-on:click="setCurrentItem(data.item)"
+              >{{ data.item.title }}</router-link>
+            </template>
+            <template slot="author" slot-scope="data" class="col-4">
+              <a :href="`#`" v-on:click="filter = data.item.author">{{ data.item.author }}</a>
+              <div style="float: right;">
+                <b-button
+                  style="vertical-align: right;"
+                  variant="primary"
+                  size="sm"
+                  :to="'item/?orgId='+$store.state.organization.id +'&itemId='+ data.item.id"
+                >
+                  <font-awesome-icon icon="pencil-alt"/>
+                </b-button>
+                <b-button
+                  style="bottom"
+                  variant="danger"
+                  size="sm"
+                  v-b-modal.delete
+                  @click="setCurrentItem(data.item)"
+                >
+                  <font-awesome-icon icon="trash-alt"/>
+                </b-button>
+              </div>
+            </template>
+          </b-table>
+        </div>
+
+        <b-modal
+          id="edit"
+          title="Edit"
+          button-size="sm"
+          size="sm"
+          centered
+          ok-variant="submit"
+          @ok="handleItemEditTitle(currentItem, newNameOldItem)"
+          ok-title="submit"
+        >
+          <b-form-input v-model="newNameOldItem" :placeholder="currentItem.title"></b-form-input>
+        </b-modal>
+        <b-modal
+          id="delete"
+          :title="'Wait. Are you sure you want to delete this permanently?'"
+          button-size="sm"
+          size="sm"
+          centered
+          body-class="zero-size"
+          ok-variant="danger"
+          @ok="handleItemDelete(currentItem)"
+          ok-title="delete"
+        ></b-modal>
+
+        <b-modal
+          id="modalnew"
+          button-size="sm"
+          title="New item"
+          @ok="handleNewItem()"
+          size="lg"
+          centered
+        >
+          <b-form-group label="Title: " label-for="title" horizontal>
+            <b-form-input v-model="newItem.title" placeholder="Title item" id="title">></b-form-input>
+          </b-form-group>
+          <div>
+            <b-form-group label="Description: " label-for="description" horizontal>
+              <b-form-textarea
+                id="description"
+                v-model="newItem.description"
+                placeholder="Title description"
+                :rows="3"
+                :max-rows="6"
+              ></b-form-textarea>
+            </b-form-group>
+          </div>
+
+          <div class="newItemtable row">
+            <div class="col">
+              <b-form-group label="Status: " label-for="labelStatus">
+                <b-dropdown
+                  :text="newItem.status.name"
+                  name="newItemStatuses"
+                  size="sm"
+                  class="statuses m-2"
+                >
+                  <b-dropdown-item
+                    v-for="element in objStatuses"
+                    v-bind:key="element.id"
+                    @click="handleItemNewItemSetField(element, 'status')"
+                    size="sm"
+                  >{{ element.name }}</b-dropdown-item>
+                </b-dropdown>
+              </b-form-group>
+            </div>
+
+            <div class="col">
+              <b-form-group label="Assignee: " label-for="newItemAssignee">
+                <b-dropdown
+                  :text="handleUsername(newItem.assignee)"
+                  name="newItemAssignee"
+                  size="sm"
+                  class="users m-2"
+                >
+                  <b-dropdown-item
+                    v-for="element in users"
+                    v-bind:key="element.userId"
+                    @click="handleItemNewItemSetField(element, 'assignee')"
+                    size="sm"
+                  >{{ handleUsername(element) }}</b-dropdown-item>
+                </b-dropdown>
+              </b-form-group>
+            </div>
+
+            <div class="col">
+              <b-form-group label="Points: " label-for="newItemPoints">
+                <b-dropdown
+                  :text="String(newItem.points)"
+                  name="newItemPoints"
+                  size="sm"
+                  class="points m-2"
+                >
+                  <b-dropdown-item
+                    v-for="element in pointsVar"
+                    v-bind:key="element"
+                    @click="handleItemNewItemSetField(element, 'points')"
+                    size="sm"
+                  >{{ element }}</b-dropdown-item>
+                </b-dropdown>
+              </b-form-group>
+            </div>
+          </div>
+        </b-modal>
+      </div>
+    </b-card>
   </v-container>
 </template>
 
@@ -231,13 +424,16 @@ export default {
       dialogNewItem: false,
       dialogItemEdit: false,
       dialogDeleteItem: false,
-      initialBItems: [],
+      initialitems: [],
       initialFilteredItems: null,
       filteredItems: null,
       objStatuses: [],
       groupByList: ["Status", "Stategic initiative"],
       currentGroupBy: "Status",
       selected: [],
+      initialSelected: [],
+      initialFilteredSelected: null,
+      filteredSelected: null,
       options: [],
       users: [],
       newItem: {
@@ -266,11 +462,15 @@ export default {
     this.selected.forEach(element => {
       this.options.push({ text: element.name, value: element });
     });
-    console.log(this.selected);
   },
 
-  computed: {},
-
+  computed: {
+    selectedCards: function() {
+      return this.filteredSelected !== null
+        ? this.filteredSelected
+        : this.selected;
+    }
+  },
   methods: {
     async reload(checked) {
       this.showArchived = checked;
@@ -324,8 +524,10 @@ export default {
           element.totalRows = element.filteredItems.length;
           element.currentPage = 1;
         });
-        if (firstLoad) this.selected = this.objStatuses;
-
+        if (firstLoad) {
+          this.selected = this.objStatuses;
+          this.initialSelected = this.objStatusesj;
+        }
         if (!firstLoad) {
           this.selected.forEach(element => {
             element.filteredItems = _.filter(this.items, {
@@ -334,6 +536,7 @@ export default {
             element.totalRows = element.filteredItems.length;
             element.currentPage = 1;
           });
+          this.initialSelected = this.selected;
         }
         this.selected.sort(function(a, b) {
           return a.id - b.id;
@@ -381,6 +584,7 @@ export default {
           element.author = username(element);
         });
         this.items = items;
+        this.initialItems = items;
       } catch (error) {
         return this.$errorMessage.show(error);
       } finally {
@@ -485,7 +689,7 @@ export default {
       });
     },
     filterItems(clickParam) {
-      let items = this.initialitems;
+      let items = this.initialSelected;
       let filterInputValue;
 
       if (typeof clickParam === "string") {
@@ -493,26 +697,31 @@ export default {
       } else {
         filterInputValue = this.filter;
       }
-
+      console.log(items);
+      console.log(filterInputValue);
       let filterKeys = ["title", "author"];
-      for (let i = 0, len = items.length; i < len; i++) {
-        this.filteredItems = items.filter(function(obj) {
-          return filterKeys.some(function(key) {
-            if (typeof obj[key] === "string" || typeof obj[key] === "number") {
-              return obj[key]
-                .toString()
-                .toLowerCase()
-                .includes(filterInputValue.toLowerCase());
-            }
-            if (typeof obj[key] === "object") {
-              return obj[key]["horizon"]
-                .toLowerCase()
-                .includes(filterInputValue.toLowerCase());
-            }
+      // this.filteredSelected = items.map(obj => {
+      //   this.filteredSelected = obj.filteredItems.filter(internalObj => {
+      //     filterKeys.some(key => {
+      //       return internalObj[key]
+      //         .toString()
+      //         .toLowerCase()
+      //         .includes(filterInputValue.toLowerCase());
+      //     });
+      //   });
+      // });
+      this.filteredSelected = items.filter(obj => {
+        for (let item of obj.filteredItems) {
+          return filterKeys.some(key => {
+            return item[key]
+              .toString()
+              .toLowerCase()
+              .includes(filterInputValue.toLowerCase());
           });
-        });
-      }
-      this.initialFilteredItems = this.filteredItems.slice();
+        }
+      });
+      console.log(this.filteredSelected);
+      this.initialFilteredSelected = this.filteredSelected.slice();
     },
     clearItemsFilter() {
       this.filteredItems = null;
